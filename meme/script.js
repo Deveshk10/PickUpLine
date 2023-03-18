476659ee240d1b62e9cddc6abc401acf899aea1a
Deveshk10 <80586334+Deveshk10@users.noreply.github.com>
2023-03-18 13:15:02 +0530

    Add files via upload

---
+++ meme/script.js
@@ -0,0 +1,8 @@
+const a = document.querySelector(".codding");
+console.log(a.innerHTML);
+const pic=document.querySelector(".pic")
+
+setTimeout(function () {
+    a.innerHTML ="Cause you've got 'fine' written all over you'"
+}, 3000)
+
